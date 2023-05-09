4299dab94817926babd13a2c9baf3ee0b6748bf8
JATENCIA <atenciajaramilloj@gmail.com>
2023-05-09 14:08:22 -0500

    add confi production

--- PI-Food-main/api/src/db.js
+++ PI-Food-main/api/src/db.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_URI } = process.env;
 
 // const sequelize = new Sequelize(`${DB_URI}`, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_URI } = process.env;
 
 let sequelize =
   process.env.NODE_ENV === "production"
