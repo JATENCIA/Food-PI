3475b5af7d5d3db227f2336e576edd1dbda6c461
JATENCIA <atenciajaramilloj@gmail.com>
2023-05-09 09:59:34 -0500

    cambios en la db

--- PI-Food-main/api/src/db.js
+++ PI-Food-main/api/src/db.js
@@ -2,15 +2,12 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_URI } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+const sequelize = new Sequelize(`${DB_URI}`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -48,3 +45,20 @@ module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
+
+// require("dotenv").config();
+// const { Sequelize } = require("sequelize");
+// const fs = require("fs");
+// const path = require("path");
+// const { DB_USER, DB_PASSWORD, DB_HOST, DB_URI } = process.env;
+
+// const sequelize = new Sequelize(
+//   // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
+//   "postgres://default:B3bpN2ywoZAV@ep-round-cake-719720.us-east-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
+//   {
+//     logging: false, // set to console.log to see the raw SQL queries
+//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   }
+// );
+
+// // Resto del código...
